94f9277e5add9ed7e78d9f8746962c8a58d5fffe
amsnks009 <amsnsskt@gmail.com>
2025-06-12 18:53:40 +0530

    folder str

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}", // for app dir
+    "./src/**/*.{js,ts,jsx,tsx}", // for app dir
     "./pages/**/*.{js,ts,jsx,tsx}", // for pages dir
     "./Component/**/*.{js,ts,jsx,tsx}", // for components
   ],
